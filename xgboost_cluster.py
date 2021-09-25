6366d5bcd19b6baef24402c18b361cdeaee9f183
CJBgon <chris.bouwens@live.nl>
2021-09-25 15:07:19 +0100

    data loading, Xysplit

--- xgboost_cluster.py
+++ xgboost_cluster.py
@@ -27,5 +27,15 @@ def Parser():
     return options
 
 def datload(dat):
-    pd.read_csv(dat, index_col=0)
+    dt = pd.read_csv(dat, index_col=0)
+    dt.dropna(axis=0, inplace=True)  # dropping patient PTP169-1, NA on the outcome.
+    y, X = dt.iloc[:, 0], dt.iloc[:, 1:]
+
+    # convert data into DMatrix:
+    dt_dmatrix = xgb.DMatrix(data=X, label=y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=45)
+    print("", Counter(y_train), Counter(y_test))
+    
+
 
