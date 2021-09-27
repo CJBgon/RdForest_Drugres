15a08b169977feeb64b55ed50688dff5f99a6ecf
CJBgon <chris.bouwens@live.nl>
2021-09-27 15:34:33 +0200

    xgboost-script for HPC cluster finished

--- xgboost_cluster.py
+++ xgboost_cluster.py
@@ -10,6 +10,10 @@ import argparse
 import pandas as pd
 import numpy as np
 import xgboost as xgb
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline as imbpipeline
 
 def Parser():
     parser = argparse.ArgumentParser()
@@ -30,12 +34,84 @@ def datload(dat):
     dt = pd.read_csv(dat, index_col=0)
     dt.dropna(axis=0, inplace=True)  # dropping patient PTP169-1, NA on the outcome.
     y, X = dt.iloc[:, 0], dt.iloc[:, 1:]
-
-    # convert data into DMatrix:
-    dt_dmatrix = xgb.DMatrix(data=X, label=y)
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=45)
-    print("", Counter(y_train), Counter(y_test))
-    
+    print("training set value counts: %s, testing set value counts: %s"%(Counter(y_train), Counter(y_test)))
+    return X_train,X_test,y_train,y_test
+
+def training(X_train, y_train, param_grid, p):
+    pipeline_xgb = imbpipeline(steps=[['smote', SMOTE(random_state=11)],
+                                      ['xgb', xgb.XGBClassifier()]
+                                      ])
+
+    grid_search_xgb = GridSearchCV(estimator=pipeline_xgb,
+                                   param_grid=param_grid,
+                                   scoring='f1',
+                                   cv=stratified_kfold,
+                                   n_jobs=p)
+
+    grid_search_xgb.fit(X_train, y_train)
+    return grid_search_xgb
 
 
+# main:
+def main():
+    options = Parser()
+
+    parameters = {'smote__sampling_strategy': [0.6, 0.7, 0.8],
+                  'smote__k_neighbors': [3, 4, 5],
+                  'xgb__n_estimators': [5, 10, 100, 200],
+                  'xgb__learning_rate': [0.01, 0.05, 0.1, 0.3, 0.5],
+                  'xgb__max_depth': [2, 4, 6, 10],
+                  'xgb__min_child_weight': [1, 2, 5],
+                  'xgb__max_delta_step': [1,5,7],
+                  'xgb__booster': ['gbtree', 'gblinear', 'dart'],
+                  'xgb__eval_metric': ['auc'],
+                  'xgb__gamma': [0, 0.2, 0.5, 0,8, 1],
+                  'xgb__reg_alpha': [0, 0.1, 0.3, 0.5, 0.7, 1],
+                  'xgb__reg_lambda': [0.1, 0.3,0.5, 1, 5, 10],
+                  'xgb__base_score': [0.2, 0.5, 1],
+                  'xgb__colsample_bytree': [0.5, 1],
+                  }
+    X_train, X_test, y_train, y_test = datload(dat = options.data)
+
+    model = training(X_train,
+                     X_test,
+                     p=options.cores,
+                     param_grid=parameters)
+
+    cv_score = model.best_score_
+    test_score = model.score(X_test, y_test)
+
+
+    print('***************************** model evalation ***************************** ')
+    print("Parameters: ", model.best_params_)
+    print(f'Cross-validation score: {cv_score}\nTest score: {test_score}')
+
+    y_train_hat = model.predict(X_train)
+    y_test_hat = model.predict(X_test)
+
+
+    print('train performance')
+    print('---------------------------------------------')
+    print(classification_report(y_train, y_train_hat))
+
+    print('Test performance')
+    print('---------------------------------------------')
+    print(classification_report(y_test, y_test_hat))
+
+    print('Roc_auc score')
+    print('---------------------------------------------')
+    print(roc_auc_score(y_test, y_test_hat))
+    print('')
+
+    print('Confusion matrix')
+    print('---------------------------------------------')
+    print(confusion_matrix(y_test, y_test_hat))
+
+    # exporting model and evaluation:
+    test_report = pd.DataFrame(classification_report(y_test, y_test_hat, output_dict=True)).transpose()
+    outfile = options.output + '/classification_report.csv'
+    test_report.to_csv(outfile)
+
+if  __name__ == "__main__":
+    main()
